fbac063fb356425d6252182373bbe86e115a804f
Yann Vaillant <vaillant.y@gmail.com>
2011-11-23 10:54:48 +0100

    version final

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def header():
+    return render_template('main.html')
+
+@app.route('/caca')
+def caca():
+    return "caca"
+
+@app.route('/caca/pipi')
+def cacapipi():
+    return "cacapipi"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
