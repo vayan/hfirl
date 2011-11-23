87c9f04e9695a3266edcb904c4c2a770207f0514
Florent Espanet <florent.esp@gmail.com>
2011-11-23 12:52:56 +0100

    add the db

--- hello.py
+++ hello.py
@@ -1,13 +1,9 @@
-from flask import Flask
+from import flask Flask
 from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-<<<<<<< HEAD
-def hello_world():
-    return 'Hello World pd!'
-=======
 def header():
     return render_template('main.html')
 
@@ -18,7 +14,6 @@ def caca():
 @app.route('/caca/pipi')
 def cacapipi():
     return "cacapipi"
->>>>>>> fbac063fb356425d6252182373bbe86e115a804f
 
 if __name__ == '__main__':
     app.run(debug=True)
