491093507503fbefa2e0e4bd5dda0d813df6ac14
Espanet Florent <florent.esp@gmail.com>
2012-12-09 15:10:23 +0100

    cleaning repo

---
+++ config.py
@@ -0,0 +1,7 @@
+CSRF_ENABLED = False
+SECRET_KEY = "foobar"
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
