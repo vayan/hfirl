e92615cd0d7cdb3696d5d82d85ca71d6d720bb5d
Espanet Florent <florent.esp@gmail.com>
2012-12-09 16:24:13 +0100

    forgot to commit new db architecture and the new register form

--- app/forms.py
+++ app/forms.py
@@ -3,3 +3,9 @@ from flask.ext.wtf import Form, TextField, Required, PasswordField
 class LoginForm(Form):
     username = TextField('username', validators = [Required()])
     password = PasswordField('password', validators = [Required()])
+
+class RegisterForm(Form):
+    username = TextField('username', validators = [Required()])
+    password = PasswordField('password', validators=[Required()])
+    email = TextField('email', validators=[Required()])
+
--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class Category(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    pseudo = db.Column(db.String(128), index = True, unique = True)
+    username = db.Column(db.String(128), index = True, unique = True)
     email = db.Column(db.String(128), unique = True)
     password = db.Column(db.String(128))
     avatar = db.Column(db.String(128))
@@ -19,11 +19,9 @@ class User(db.Model):
     rank = db.Column(db.Integer)
     date_sigin = db.Column(db.Date)
     date_online = db.Column(db.Date)
-    created_achievements = db.relationship('Achievement', backref = 'creator', lazy = 'dynamic')
-    achievements = db.relationship('UserAchieved', backref = 'user', lazy = 'dynamic')
 
     def __repr__(self):
-        return '<User %r # %r>' % (self.pseudo, self.email)
+        return '<User %r # %r>' % (self.username, self.email)
 
 class Achievement(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -31,19 +29,18 @@ class Achievement(db.Model):
     category = db.Column(db.Integer)
     image = db.Column(db.String(128))
     description = db.Column(db.String(512))
-    creator = db.Column(db.Integer, db.ForeignKey('user.id'))
+    creator = db.Column(db.Integer)
     date_created = db.Column(db.Date)
     point_worth = db.Column(db.Integer)
     score_vote = db.Column(db.Integer)
-    users = db.relationship('UserAchieved', backref = 'achievements', lazy = 'dynamic')
 
     def __repr__(self):
         return '<Achievement %r>' % (self.name)
 
 class UserAchieved(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    achievement = db.Column(db.Integer, db.ForeignKey('achievement.id'))
-    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    achievement = db.Column(db.Integer)
+    user = db.Column(db.Integer)
 
     def __repr__(self):
         return '<UserAchieved %r > %r>' % (user, achievement)
